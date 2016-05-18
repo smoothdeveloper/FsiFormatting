04afaf5c417142a252f61f596457c8d253a11f80
Gauthier Segay <gauthier.segay@gmail.com>
2016-05-18 03:12:18 +0200

    crude definition of Result module, with a sample for DataTable formatter in a separate script

---
+++ src/FsiFormatting/FsiFormatting.fs
@@ -0,0 +1,40 @@
+﻿namespace FsiFormatting
+open System
+
+type internal ImageOutput =
+| Raw of byte array
+| External of Uri
+
+type internal SvgOutput = 
+| Raw of string
+| External of Uri
+
+type internal HtmlOutput = { Data: string }
+type internal JsonOutput = { Data: obj }
+
+type internal Output =
+| JSON of JsonOutput
+| Html of HtmlOutput
+| Svg of SvgOutput
+| Binary of byte array
+| Opaque of obj
+
+type Result =
+  private {
+    ContentType: string
+    Contents:    Output
+  }
+
+module Html =
+  open System.Web
+  let internal htmlEncode(str) = HttpUtility.HtmlEncode(str)
+
+[<RequireQualifiedAccess; CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module Result =
+  let makeHtml contents      = { ContentType = "text/html"; Contents = Html { HtmlOutput.Data = contents } }
+  let getHtmlContents result =
+    match result.Contents with
+    | Html output -> Some output.Data
+    | _ -> None
+
+type 't formatter = 't -> Result
---
+++ src/FsiFormatting/Script.fsx
@@ -0,0 +1,45 @@
+﻿#load "FsiFormatting.fs"
+open FsiFormatting
+
+module Printers =
+  open Html
+  open System.Text
+  let dataTable (table: System.Data.DataTable) =
+    let builder = StringBuilder()
+    let append (text: string) = builder.Append text |> ignore
+    let appendHtmlEncode (text: obj) = text |> string |> htmlEncode |> append
+
+    append "<table>"
+
+    // output header
+    append "<thead>"
+    append "<tr>"
+    for column in table.Columns do
+      append "<th>"
+      appendHtmlEncode column.ColumnName
+      append "</th>"
+    append "</tr>"
+    append "</thead>"
+
+    // output body
+    append "<tbody>"
+    for row in table.Rows do
+      append "<tr>"
+      for cell in row.ItemArray do
+        append "<td>"
+        appendHtmlEncode cell
+        append "</td>"
+
+      append "</tr>"
+    append "<tbody>"
+    append "</tbody>"
+    append "</table>"
+    Result.makeHtml (string builder)
+
+let table = new System.Data.DataTable()
+table.Columns.Add("a") |> ignore
+[1..100] |> Seq.iter(fun i -> table.Rows.Add(box i) |> ignore)
+
+Printers.dataTable table
+|> Result.getHtmlContents
+|> Option.iter (printfn "%s")
